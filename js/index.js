a3d4d79e9eb34cce5518228c67fdd9b3466edaae
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-05-23 13:34:42 -0400

    Initial Interface Build

---
+++ js/index.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+    login()
+})
+
+const login = () => {
+    const loginId = document.getElementById('loginForm')
+    loginId.addEventListener('submit', e => {
+            if (e.target.id == 'loginForm'){
+            e.target.value = ""
+            const loginScreen = document.getElementById('loginScreen')
+            loginScreen.style.zIndex = 1
+            loginScreen.style.display = "none"
+                
+        }
+    })  
+}
+
+document.addEventListener('click', e => {
+    if (e.target.id == "systemApple" && e.target.dataset.id == "hide"){
+        e.target.dataset.id = "show"
+        const appleIcon = document.getElementById("logOut").style.display = "block"
+    }else if (e.target.id == "systemApple" && e.target.dataset.id == "show"){
+        e.target.dataset.id = "hide"
+        const appleIcon = document.getElementById("logOut").style.display = "none"
+    }else if(e.target.id == "logOutBtn"){
+        e.target.parentElement.dataset.id = "hide"
+        e.target.parentElement.style.display = "none"
+        const loginScreen = document.getElementById('loginScreen')  
+        loginScreen.style.zIndex = 25
+        loginScreen.style.display = "block"
+    }else if (e.target.dataset.id == "closed"){
+      e.target.dataset.id = "open"
+      document.getElementById("mySidebar").style.transform = "translateX(-100%)"
+    }else if (e.target.dataset.id == "open"){
+      e.target.dataset.id = "closed"
+      document.getElementById("mySidebar").style.transform = "translateX(0)"
+    }
+})
+
+const displayWidgets = () => {
+    
+}
+
+
+const divOpen1 = () => {
+    
+    
+}
