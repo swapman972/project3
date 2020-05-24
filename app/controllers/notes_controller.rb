9f17787c14238516e4fb00bfdc8a78aa263e6f66
swapman972 <jordan.melidor@hotmail.fr>
2020-05-23 20:34:40 -0400

    note delete in progress

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -31,6 +31,7 @@ class NotesController < ApplicationController
     def destroy
         @note = Note.find(params[:id])
         @note.destroy
+        render json: @note, except: [:created_at, :updated_at]
     end
 
        private
