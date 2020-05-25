54265e230d5d29251251c0c5588a8b6fb2269648
swapman972 <jordan.melidor@hotmail.fr>
2020-05-25 03:31:02 -0400

    pokedex

--- js/pokedex.js
+++ js/pokedex.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const fetchPokemon = () => {
         const promises = [];
-        for (let i = 1; i <= 150; i++){
+        for (let i = 1; i <= 151; i++){
             const url = `https://pokeapi.co/api/v2/pokemon/${i}`
             promises.push(fetch(url).then((res) => res.json()))
         }
